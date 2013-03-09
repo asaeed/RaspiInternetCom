394318f09318e2339833ad936353ad312e855e48
Ahmad Saeed <asaeed@gmail.com>
2013-03-08 22:03:58 -0500

    first

--- raspi.py
+++ raspi.py
@@ -1,18 +1,20 @@
 
 from ws4py.client.threadedclient import WebSocketClient
 
-class EchoClient(WebSocketClient):
+class RaspiClient(WebSocketClient):
      def opened(self):
+        self.send("Hi there!")
          print "Connection opened..."
 
      def closed(self, code, reason=None):
-         print code, reason
+         print "Connection closed... ", code, reason
 
      def received_message(self, m):
-         self.send(m)
+         print "Received from server: %s" % (str(m))
 
 try:
     ws = EchoClient('ws://ec2-23-20-219-99.compute-1.amazonaws.com:8080/ws')
     ws.connect()
 except KeyboardInterrupt:
     ws.close()
+
